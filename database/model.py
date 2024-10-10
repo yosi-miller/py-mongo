557333a41a760db21c3f5c0ed20a2657edc24be8
Yossi Miller <y0504199881@gmail.com>
2024-10-10 12:43:19 +0300

    create filed index

--- database/model.py
+++ database/model.py
@@ -1,25 +1,42 @@
-def crash_document(row):
+from datetime import datetime
+
+
+def parse_date(date_str: str):
+    has_seconds = len(date_str.split(' ')) > 2
+    date_format = '%m/%d/%Y %H:%M:%S %p' if has_seconds else '%m/%d/%Y %H:%M'
+    return datetime.strptime(date_str, date_format)
+
+
+def injuries_info(row):
+    injuries = {
+        'total': row['INJURIES_TOTAL'],
+        'injuries_status': {
+            'fatal': row['INJURIES_FATAL'],
+            'incapacitating': row['INJURIES_INCAPACITATING'],
+            'non_incapacitating': row['INJURIES_NON_INCAPACITATING']
+        }
+    }
+
+    return injuries
+
+
+def crash_document(row, injuries_info_id):
     crash_cause = {
         'prim': row['PRIM_CONTRIBUTORY_CAUSE'],
         'sec': row['SEC_CONTRIBUTORY_CAUSE']
     }
 
-    injuries_info = {
-        'total': row['INJURIES_TOTAL'],
-        'injuries_status': {
-            'fatal': row['INJURIES_FATAL'],
-            'incapacitating': row['INJURIES_INCAPACITATING'],
-            'non incapacitating': row['INJURIES_NON_INCAPACITATING']
-        }
-    }
     crash = {
-        'date': row['CRASH_DATE'],
+        'date': parse_date(row['CRASH_DATE']),
         'beat': row['BEAT_OF_OCCURRENCE'],
-        'day of week': row['CRASH_DAY_OF_WEEK'],
+        'day_of_week': row['CRASH_DAY_OF_WEEK'],
         'month': row['CRASH_MONTH'],
         'crash_cause': crash_cause,
-        'injuries_info': injuries_info
+        'injuries_info': injuries_info_id
     }
 
     return crash
 
+if __name__ == '__main__':
+    t = parse_date('09/22/2023 06:45:00 PM')
+    print(type(t))
--- repository/csv_repository.py
+++ repository/csv_repository.py
@@ -1,7 +1,7 @@
 import csv
 from pymongo import errors
 from database.connect import get_db
-from database.model import crash_document
+from database.model import crash_document, injuries_info
 from services.logger import log_error, log_info
 
 
@@ -19,18 +19,37 @@ def init_crash_information_from_csv():
 
     client, db = get_db()
 
-    collection = db['crash information']
+    crash_collection = db['crash information']
+    injuries_collection = db['injuries information']
 
-    if collection.count_documents({}) == 0:
+    if crash_collection.count_documents({}) == 0:
         try:
             for row in read_csv('data/Traffic_Crashes_-_Crashes - 20k rows.csv'):
-                document = crash_document(row)
-                collection.insert_one(document)
+                injuries_document = injuries_info(row)
+
+                injuries_id = injuries_collection.insert_one(injuries_document).inserted_id
+
+                document = crash_document(row, injuries_id)
+
+                crash_collection.insert_one(document)
 
             log_info(f'action: completed insert crashs information to db')
+
+            create_index(crash_collection, injuries_collection)
+
         except errors.PyMongoError as e:
             log_error(f'action: try insert crashs information, error: {e}')
             print(f'Error: {e}')
             return e
         finally:
             client.close()
+
+
+def create_index(crash_collection, injuries_collection):
+    # create a new index for the crash document and injustice document
+    crash_collection.create_index('crash_cause')
+    crash_collection.create_index('beat')
+    crash_collection.create_index('date')
+    injuries_collection.create_index('total')
+    injuries_collection.create_index('injuries_status')
+    log_info(f'action: completed create indexes for crashs and injuries collections')
