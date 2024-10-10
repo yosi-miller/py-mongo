32482fdbea0bf3c29553fa5aa043170a756dc96e
Yossi Miller <y0504199881@gmail.com>
2024-10-10 17:04:16 +0300

    completed get_crash_by_area_and_season route

--- routes/crash_route.py
+++ routes/crash_route.py
@@ -5,14 +5,12 @@ crash_bp = Blueprint('post', __name__, url_prefix='/crash')
 
 @crash_bp.route('/area/<string:area>/')
 def get_crash_by_area(area):
-    users = find_crash_by_area(area)
-    return jsonify(users), 201 if users else 404
+    area = find_crash_by_area(area)
+    return jsonify(area), 201 if len(area) > 1 else 404
 
-
-@crash_bp.route('/season/<string:area>/', methods=['GET'])
-def get_crash_by_area_and_season(area):
-    season = request.json()
-    posts = find_crash_by_area_and_season(area, season)
+@crash_bp.route('/season/<string:area>/<string:date>/<string:range_search>/', methods=['GET'])
+def get_crash_by_area_and_season(area, date, range_search):
+    posts = find_crash_by_area_and_season(area, date, range_search)
     return jsonify(posts), 201 if posts else 404
 
 
@@ -26,3 +24,5 @@ def get_crash_by_group():
 def get_injuries_statistics():
     stats = find_injuries_statistics()
     return jsonify(stats), 201 if stats else 404
+
+# TODO: לבדוק מה חוזר אם יש מידע ריק
--- services/posts_server.py
+++ services/posts_server.py
@@ -1,13 +1,13 @@
+from datetime import datetime, timedelta
+
 from database.connect import get_db
 from services.logger import log_info, log_error
 
 
 def find_crash_by_area(area):
     client, db = get_db()
-    print(area)
     try:
         crashs = list(db['crash information'].find({'beat': area}, {'_id': 0, 'injuries_info': 0}))
-        print(crashs)
         log_info('get all crashs from db')
         return crashs
     except Exception as e:
@@ -18,19 +18,41 @@ def find_crash_by_area(area):
         client.close()
 
 
-def find_crash_by_area_and_season(area, season):
+def find_crash_by_area_and_season(area, date, range_search):
     client, db = get_db()
-
+    # TODO:לחלק את השאלתיה ל 2 חלקים
     try:
-        print('get all crash')
-        crashs = list(db['crash information'].find({'bead': area,'season': season}, {'_id': 0}))
-        # users = list(db.posts.find({'userId': user_id}, {'_id': 0}))
-        # log_info('get all users posts_by_user_id from db')
-        return crashs
+        time_periods = {
+            "day": 1,
+            "week": 7,
+            "month": 30
+        }
+
+        add_date = time_periods.get(range_search)
+
+        start_date = datetime.strptime(date, '%m-%d-%Y')
+        end_date = start_date + timedelta(days=add_date)
+
+        sum_crash = db['crash information'].count_documents(
+            {'beat': area,
+             "date": {"$gte": start_date, "$lt": end_date}})
+
+        result = {
+            "start_date": start_date,
+            "end_date": end_date,
+            "area": area,
+            "range_search": range_search,
+            "total_crashes": sum_crash
+        }
+
+        log_info('get crashs from db by area and season')
+
+        return result
     except Exception as e:
-        # log_error(f'action: get all users posts_by_user_id from db, error: {e}')
+        log_error(f'action: get crashs from db by area and season, error: {e}')
         print(f'Error: {e}')
         return e
+
     finally:
         client.close()
 
